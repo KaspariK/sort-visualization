29ed8ffd37c265b7cacd82fa98b462fbe07e4126
Kevin Kaspari <KKASPARI@goremutual.ca>
2020-10-25 16:02:49 -0400

    Add basic svg styling to mostly center align the bars. Update state handler so that it isn't one step behind on bar count. Add better width measurement for svg, although this is still sloppy

--- src/components/graph.js
+++ src/components/graph.js
@@ -3,33 +3,49 @@ import randomArray from "../utils/array";
 
 function Graph() {
   const [items, setItems] = useState([]);
-  let space = 10;
-  const width = 300;
-  const height = 110;
+  const [count, setCount] = useState(10);
+
+  let space = 20;
+  const height = 100;
+  const width = 25;
 
   useEffect(() => {
-    setItems(randomArray(10, 100));
-  }, []);
+    handleArrayGen();
+  }, [count]);
+
+  const handleArrayGen = () => {
+    let testArr = randomArray(count, 100);
+    setItems(testArr);
+  };
 
   return (
     <>
-      <button onClick={() => setItems(randomArray(10, 100))}>
-        Generate Array
-      </button>
-
-      <svg viewBox={`0 0 ${width} ${height}`}>
-        {items.map((item) => {
-          return (
-            <rect
-              x={(space += 20)}
-              y={height - item - 2}
-              width="10"
-              height={item + 2}
-              fill="#348AA7"
-            />
-          );
-        })}
-      </svg>
+      <div>
+        <button onClick={handleArrayGen}>Generate Array</button>
+        <input
+          type="range"
+          min="10"
+          max="20"
+          value={count.toString()}
+          onChange={(e) => setCount(parseInt(e.target.value))}
+        />
+      </div>
+      <div className="container">
+        <svg width={count * width}>
+          {items.map((item) => {
+            return (
+              <rect
+                key={space}
+                x={(space += 20)}
+                y={height - item - 2}
+                width="10"
+                height={item + 2}
+                fill="#348AA7"
+              />
+            );
+          })}
+        </svg>
+      </div>
     </>
   );
 }
